54d721d2b60081f8b7d9e998a1a2c829926b28b4
dhtjddls <dhtjddls12@naver.com>
2023-05-25 15:32:13 +0900

    feature #7 기본 세팅

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,53 +1,54 @@
 import {
-    BaseEntity,
-    Column,
-    Entity,
-    PrimaryGeneratedColumn,
-    Unique,
-    OneToMany,
-    CreateDateColumn,
-    UpdateDateColumn,
-    ManyToOne,
-    joinColumn
-
-} from "typeorm";
-import { Notification } from "../notifications/notification.entity";
-import { Like } from "../likes/like.entity";
-import { User } from "../auth/user.entity";
-import { Movie } from "../movies/movie.entity";
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../auth/user.entity';
+import { Like } from '../likes/like.entity';
+import { Movie } from '../movies/movie.entity';
+import { Notification } from '../notifications/notification.entity';
 
 @Entity()
 @Unique(['email', 'nickname'])
 export class Post extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    postId: number;
+  @PrimaryGeneratedColumn()
+  postId: number;
 
-    @Column()
-    content: string;
+  @Column()
+  content: string;
 
-    @Column()
-    comment: string;
+  @Column()
+  comment: string;
 
-    @Column()
-    version: number;
+  @Column()
+  version: number;
 
-    @CreateDateColumn({ type: 'timestamp' })
-    createdAt: Date;
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
 
-    @UpdateDateColumn({ type: 'timestamp' })
-    updatedAt: Date;
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
 
-    @OneToMany(type => Notification, Notification => Notification.post, { eager: true })
-    notifications: Notification[];
+  @OneToMany((type) => Notification, (Notification) => Notification.post, {
+    eager: true,
+  })
+  notifications: Notification[];
 
-    @OneToMany(type => Like, like => like.post, { eager: true })
-    likes: Like[];
+  @OneToMany((type) => Like, (like) => like.post, { eager: true })
+  likes: Like[];
 
-    @ManyToOne(() => User, (user) => user.posts, { eager: false})
-    @joinColumn({ name: 'userId', referencedColumnName: 'userId' })
-    user: User;
+  @ManyToOne(() => User, (user) => user.posts, { eager: false })
+  @JoinColumn({ name: 'userId', referencedColumnName: 'userId' })
+  user: User;
 
-    @ManyToOne(() => User, (movie) => movie.posts, { eager: false})
-    @joinColumn({ name: 'movieId', referencedColumnName: 'movieId' })
-    movie: Movie;
+  @ManyToOne(() => User, (movie) => movie.posts, { eager: false })
+  @JoinColumn({ name: 'movieId', referencedColumnName: 'movieId' })
+  movie: Movie;
 }
