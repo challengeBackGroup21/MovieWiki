7c481c4807ed228f20dc4565e21ffe7eb0b18f67
dhtjddls <dhtjddls12@naver.com>
2023-05-25 15:35:40 +0900

    feat: #7 기본 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { LikesModule } from './likes/likes.module';
 import { MoviesModule } from './movies/movies.module';
@@ -15,7 +13,7 @@ import { PostsModule } from './posts/posts.module';
     NotificationsModule,
     PostsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
