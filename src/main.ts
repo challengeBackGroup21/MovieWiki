6bfe2548d6c0810a09ebc45a1b87e9bf59480e52
dhtjddls <dhtjddls12@naver.com>
2023-05-26 17:15:17 +0900

    featuer #7 httpExceptionFilter 추가 error 발생 시 throw new HttpException(statusCode, message) 형식으로 사용

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as config from 'config';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server');
   const port = serverConfig.port;
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(port);
   Logger.log(`Application running on port ${port}`);
 }
