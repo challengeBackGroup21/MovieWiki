ac901bba59c65859a1c5c0a443edf751eb6874fc
dhtjddls <dhtjddls12@naver.com>
2023-05-26 17:05:03 +0900

    feature #7 logger 추가, nest/config package 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { typeORMConfig } from './configs/typeorm.config';
 import { LikesModule } from './likes/likes.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
 import { MoviesModule } from './movies/movies.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(typeORMConfig),
+
     AuthModule,
     LikesModule,
     MoviesModule,
@@ -16,4 +25,8 @@ import { PostsModule } from './posts/posts.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import * as config from 'config';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const serverConfig = config.get('server');
   const port = serverConfig.port;
   await app.listen(port);
