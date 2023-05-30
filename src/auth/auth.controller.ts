b2f32dfd19bc7ee127760ec18e71e40bc4b1fe1a
DongHwna Lee <meadding231@gmail.com>
2023-05-30 09:25:51 +0900

    [init] auth 기능 세팅, npm 라이브러리 설치

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('signup')
+  signUp(
+    @Body(ValidationPipe) AuthCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
+    return this.authService.signUp(AuthCredentialsDto);
+  }
+
+  @Post('/signin')
+  signIn(
+    @Body(ValidationPipe) AuthCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(AuthCredentialsDto);
+  }
+
+  @Post('/authtest')
+  @UseGuards(AuthGuard())
+  test(@GetUser() user: User) {
+    console.log('user', user);
+  }
+
   @Get('test')
-  signUp() {
+  test2() {
     return 'auth_test 2';
   }
 }
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,35 @@
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.manager);
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { email, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ email, password: hashedPassword });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
