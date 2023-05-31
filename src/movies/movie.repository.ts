90b60215965432e57be0a8a7f9e6a4fe90ad6169
namu56 <swi920224@gmail.com>
2023-05-31 21:50:16 +0900

    영화 검색, 영화 상세 페이지 조회, 영화 상세 정보 수정 api 작성

--- src/movies/movie.repository.ts
+++ src/movies/movie.repository.ts
@@ -1,10 +1,64 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { Movie } from './movie.entity';
+import { UpdateMovieDto } from './dto/update-movie-dto';
 
 @Injectable()
 export class MovieRepository extends Repository<Movie> {
   constructor(private dataSource: DataSource) {
     super(Movie, dataSource.createEntityManager());
+  }
+  async directorsSearch(option: string, query: string): Promise<Movie[]> {
+    const movies = await this.createQueryBuilder('movie')
+      .where('movie.directors @> :directors', {
+        directors: JSON.stringify([{ peopleNm: query }]),
+      })
+      .getMany();
+    return movies;
+  }
+
+  async genreAltSearch(option: string, query: string): Promise<Movie[]> {
+    const movies = await this.createQueryBuilder('movie')
+      .where('movie.genreAlt LIKE :genreAlt', { genreAlt: `%${query}%` })
+      .getMany();
+
+    return movies;
+  }
+
+  async nationAltSearch(option: string, query: string): Promise<Movie[]> {
+    const movies = await this.createQueryBuilder('movie')
+      .where('movie.nationAlt LIKE :nationAlt', { nationAlt: `%${query}%` })
+      .getMany();
+
+    return movies;
+  }
+
+  async openDtSearch(option: string, query: string): Promise<Movie[]> {
+    const movies = await this.createQueryBuilder('movie')
+      .where('movie.openDt LIKE :openDt', { openDt: `${query}%` })
+      .getMany();
+
+    return movies;
+  }
+
+  async movieNmSearch(option: string, query: string): Promise<Movie[]> {
+    const movies = await await this.find({
+      where: { movieNm: query },
+    });
+
+    return movies;
+  }
+
+  async getMovieById(movieId: number): Promise<Movie> {
+    const isExistMovie = await this.findOne({
+      where: { movieId },
+    });
+
+    return isExistMovie;
+  }
+
+  async updateMovieData(updateMovie): Promise<Movie> {
+    await this.save(updateMovie);
+    return updateMovie;
   }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,11 +1,128 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { MovieRepository } from './movie.repository';
+import { Movie } from './movie.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UpdateMovieDto } from './dto/update-movie-dto';
 
 @Injectable()
 export class MoviesService {
-  constructor(private movieRepositry: MovieRepository) {}
-  //   async createMovieRecord(
-  //     createMovieRecordDto: CreateMovieRecordDto,
-  //     movieId: number,
-  //   ) {}
+  constructor(
+    @InjectRepository(MovieRepository)
+    private movieRepositry: MovieRepository,
+  ) {}
+
+  // option에 따라 검색하기
+  async search(option: string, query: string): Promise<Movie[]> {
+    try {
+      if (option === 'directors') {
+        // QueryBuilder를 사용하여 directors 필드 내의 peopleNm을 검색
+        const movies = await this.movieRepositry.directorsSearch(option, query);
+
+        if (movies.length === 0) {
+          throw new HttpException(
+            `${query}감독님에 해당하는 영화 목록 조회를 실패했습니다`,
+            400,
+          );
+        }
+
+        return movies;
+      }
+
+      if (option === 'genreAlt') {
+        const movies = await this.movieRepositry.genreAltSearch(option, query);
+
+        if (movies.length === 0) {
+          throw new HttpException(
+            `${query} 장르에 해당하는 영화 목록 조회를 실패했습니다`,
+            400,
+          );
+        }
+
+        return movies;
+      }
+
+      if (option === 'nationAlt') {
+        const movies = await this.movieRepositry.nationAltSearch(option, query);
+
+        if (movies.length === 0) {
+          throw new HttpException(
+            `${query} 국가에 해당하는 영화 목록 조회를 실패했습니다`,
+            400,
+          );
+        }
+
+        return movies;
+      }
+
+      if (option === 'openDt') {
+        const movies = await this.movieRepositry.openDtSearch(option, query);
+
+        if (movies.length === 0) {
+          throw new HttpException(
+            `${query} 년도에 해당하는 영화 목록 조회를 실패했습니다`,
+            400,
+          );
+        }
+
+        return movies;
+      }
+
+      if (option === 'movieNm') {
+        // 동일한 제목의 영화 존재할 수 도 있어서 find로 검색
+        const movies = await this.movieRepositry.movieNmSearch(option, query);
+        if (movies.length === 0) {
+          throw new HttpException(
+            `${query}에 해당하는 영화 조회에 실패했습니다`,
+            400,
+          );
+        }
+
+        return movies;
+      }
+
+      // if (!option) {
+      //   const movies = await this.movieRepositry.find({
+      //     order: {
+      //       movieId: 'ASC',
+      //     },
+      //     take: 20,
+      //   });
+
+      //   return movies;
+      // } else {
+      //   throw new HttpException('영화 조회에 실패하였습니다', 400);
+      // }
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      } else {
+        throw new HttpException('영화 조회에 실패하였습니다', 400);
+      }
+    }
+  }
+
+  // 영화 상세 정보 검색하기
+  async getMovieById(movieId: number): Promise<Movie> {
+    const isExistMovie = await this.movieRepositry.getMovieById(movieId);
+
+    if (!isExistMovie) {
+      throw new HttpException('존재하지 않는 영화입니다', 400);
+    }
+
+    return isExistMovie;
+  }
+
+  // 영화 상세 정보 수정하기
+  async updateMovieData(
+    movieId: number,
+    updateMovieDto: UpdateMovieDto,
+  ): Promise<Movie> {
+    if (Object.keys(updateMovieDto).length === 0) {
+      throw new HttpException('변경할 영화 정보를 입력해주세요', 400);
+    }
+    const movie = await this.getMovieById(movieId);
+    const updateMovie = { ...movie, ...updateMovieDto };
+
+    return await this.movieRepositry.save(updateMovie);
+  }
 }
