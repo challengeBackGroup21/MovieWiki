d5ea30c0c68299d9fe560e435beac5ef5b8b89b5
redjun89 <jjune1816@gmail.com>
2023-06-01 20:09:18 +0900

    불필요한 주석 및 import 제거

--- src/likes/like.repository.ts
+++ src/likes/like.repository.ts
@@ -2,15 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Like } from './like.entity';
-import { Movie } from '.././movies/movie.entity';
 
 @Injectable()
 export class LikeRepository {
     constructor(
         @InjectRepository(Like)
         private readonly likeRepository: Repository<Like>,
-        @InjectRepository(Movie)
-        private readonly movieRepository: Repository<Movie>
     ) { }
 
     async findOneLike(movieId: number, userId: number) {
@@ -26,13 +23,5 @@ export class LikeRepository {
 
     async destroyLike(movieId: number, userId: number) {
         await this.likeRepository.delete({ movieId, userId });
-    }
-
-    async incrementMovieLike(movieId: number) {
-        await this.movieRepository.increment({ movieId }, "likes", 1);
-    }
-
-    async decrementMovieLike(movieId: number) {
-        await this.movieRepository.decrement({ movieId }, "likes", 1);
     }
 };
