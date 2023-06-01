37f785312cc64aeacd60a38ff6f89173ad86ffdc
redjun89 <jjune1816@gmail.com>
2023-06-01 16:23:54 +0900

    feature #6 신고 기능 구현

---
+++ src/notifications/notification-status.enum.ts
@@ -0,0 +1,5 @@
+export enum NotificationStatus {
+    AWAIT = 'AWAIT',
+    ACCEPT = 'ACCEPT',
+    REJECT = 'REJECT',
+}
---
+++ src/notifications/pipe/notification-status-validation.pipe.ts
@@ -0,0 +1,25 @@
+import { HttpException, PipeTransform } from "@nestjs/common";
+import { NotificationStatus } from '../notification-status.enum'
+
+export class NotificationStatusValidationPipe implements PipeTransform {
+    readonly StatusOptions = [
+        NotificationStatus.AWAIT,
+        NotificationStatus.ACCEPT,
+        NotificationStatus.REJECT
+    ]
+
+    transform(value: any){
+        value = value.toUpperCase();
+
+        if(!this.isStatusValid(value)) {
+            throw new HttpException(`${value} 올바른 옵션이 아닙니다.`, 401)
+        }
+
+        return value;
+    }
+
+    private isStatusValid(status: any) {
+        const index = this.StatusOptions.indexOf(status);
+        return index !== -1
+    }
+}
