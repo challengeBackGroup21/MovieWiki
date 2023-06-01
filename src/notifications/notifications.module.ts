dea4e1733a0ebe6fc86ccbf3f6b2f027ad65475f
redjun89 <jjune1816@gmail.com>
2023-06-01 20:08:07 +0900

    repository 연결 수정

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import { NotificationRepository } from './notification.repository';
+import { Notification } from './notification.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Notification])],
   controllers: [NotificationsController],
-  providers: [NotificationsService]
+  providers: [NotificationsService, NotificationRepository],
+  exports: [NotificationsService, NotificationRepository],
 })
 export class NotificationsModule {}
