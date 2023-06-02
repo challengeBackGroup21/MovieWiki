efc9f1c49c27a81466a5253585979602f5ae9744
DongHwna Lee <meadding231@gmail.com>
2023-06-02 21:01:59 +0900

    [fix] jwt 암호화 키 환경변수로 변경

--- src/auth/constans.ts
+++
@@ -1,6 +0,0 @@
-export const jwtConstants = {
-  atExpiresIn: '2h',
-  rtExpiresIn: '7d',
-  atSecret: 'Hanghae99Team21ProjectAccessTokenSecret',
-  rtSecret: 'Hanghae99Team21ProjectRefreshTokenSecret',
-};
