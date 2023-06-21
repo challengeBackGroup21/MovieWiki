ee7f5b853dcbe0b3d5ab850465a5a7784e5e5a38
DongHwna Lee <meadding231@gmail.com>
2023-06-21 21:23:31 +0900

    [test] updateLike, createPost 테스트 코드 작성

--- src/posts/dto/create-post-record.dto.ts
+++ src/posts/dto/create-post-record.dto.ts
@@ -1,4 +1,6 @@
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { AuthUser } from 'src/auth/dto/auth-user.oo';
+import { QueryRunner } from 'typeorm';
 
 export class CreatePostRecordDto {
   @IsNotEmpty()
@@ -12,4 +14,18 @@ export class CreatePostRecordDto {
   @IsOptional()
   @IsNumber()
   version: number;
+}
+
+export class CreatePostOo {
+  @IsNotEmpty()
+  createPostRecordDto: CreatePostRecordDto;
+
+  @IsOptional()
+  movieId: number;
+
+  @IsOptional()
+  user;
+
+  @IsOptional()
+  queryRunner: QueryRunner;
 }
--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -4,11 +4,14 @@ import { PostRepository } from './post.repository';
 import { MovieRepository } from '../movies/movie.repository';
 import { SnapshotRepository } from '../snapshot/snapshot.repository';
 import { CurrentSnapshotRepository } from '../current-snapshot/current-snapshot.repository';
-import { DataSource } from 'typeorm';
+import { DataSource, QueryRunner } from 'typeorm';
 import { CreatePostRecordDto } from './dto/create-post-record.dto';
 
 describe('PostsService', () => {
   let postService: PostService;
+  let movieRepository: MovieRepository;
+  let postRepository: PostRepository;
+  let csRepository: CurrentSnapshotRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,11 +19,14 @@ describe('PostsService', () => {
         PostService,
         {
           provide: PostRepository,
-          useValue: {},
+          useValue: {
+            getLatestPostRecord: jest.fn(),
+            createPostRecord: jest.fn(),
+          },
         },
         {
           provide: MovieRepository,
-          useValue: {},
+          useValue: { findOneMovie: jest.fn() },
         },
         {
           provide: SnapshotRepository,
@@ -28,7 +34,11 @@ describe('PostsService', () => {
         },
         {
           provide: CurrentSnapshotRepository,
-          useValue: {},
+          useValue: {
+            findOneCurrentSnapshot: jest.fn(),
+            createCurrentSnapshot: jest.fn(),
+            updateCurrentSnapshot: jest.fn(),
+          },
         },
         {
           provide: DataSource,
@@ -38,31 +48,184 @@ describe('PostsService', () => {
     }).compile();
 
     postService = module.get<PostService>(PostService);
+    movieRepository = module.get<MovieRepository>(MovieRepository);
+    postRepository = module.get<PostRepository>(PostRepository);
+    csRepository = module.get<CurrentSnapshotRepository>(
+      CurrentSnapshotRepository,
+    );
   });
 
   it('should be defined', () => {
     expect(postService).toBeDefined();
   });
 
-  /** 좀 나눠야 할 것 같기도 하다. */
-  // it('createPostRecord success', () => {
-  //   // given
-  //   const createPostRecordDto: CreatePostRecordDto = {
-  //     content: '',
-  //     comment: 'ㅇㅇ',
-  //     version: null,
-  //   };
+  it('createPostRecord success', async () => {
+    // given
+    const createPostRecordDto: CreatePostRecordDto = {
+      content: '',
+      comment: 'ㅇㅇ',
+      version: null,
+    };
 
-  //   const movieId = 1;
-  //   const user = {
-  //     userId: 1,
-  //     email: '',
-  //     nickname: '',
-  //     auth: 'USER',
-  //   };
+    const movieId = 1;
+    const user = {
+      userId: 1,
+      email: '',
+      nickname: '',
+      auth: 'USER',
+    };
 
-  //   // when
-  //   postService.createPostRecord(createPostRecordDto, movieId, user);
-  //   // then
-  // });
+    postService.makeQueryRunner = jest.fn();
+    postService.createPostWithRollback = jest.fn();
+
+    // when
+    await postService.createPostRecord(createPostRecordDto, movieId, user);
+
+    // then
+    expect(postService.makeQueryRunner).toHaveBeenCalledTimes(1);
+    expect(postService.createPostWithRollback).toHaveBeenCalledTimes(1);
+  });
+
+  it('createPostWithRollback success', async () => {
+    // given
+    const createPostRecordDto: CreatePostRecordDto = {
+      content: '',
+      comment: 'ㅇㅇ',
+      version: null,
+    };
+    const movieId = 1;
+    const user = {
+      userId: 1,
+      email: '',
+      nickname: '',
+      auth: 'USER',
+    };
+
+    const mockQueryRunner = {
+      rollbackTransaction: jest.fn(),
+      release: jest.fn(),
+    } as unknown as QueryRunner;
+
+    postService.savePostAndSnapshot = jest.fn();
+
+    // when
+    await postService.createPostWithRollback(
+      createPostRecordDto,
+      movieId,
+      user,
+      mockQueryRunner,
+    );
+
+    // then
+    expect(postService.savePostAndSnapshot).toHaveBeenCalledTimes(1);
+    expect(mockQueryRunner.rollbackTransaction).toHaveBeenCalledTimes(0);
+    expect(mockQueryRunner.release).toHaveBeenCalledTimes(1);
+  });
+
+  it('savePostAndSnapshot success', async () => {
+    // given
+    const createPostRecordDto: CreatePostRecordDto = {
+      content: '',
+      comment: 'ㅇㅇ',
+      version: null,
+    };
+    const movieId = 1;
+    const user = {
+      userId: 1,
+      email: '',
+      nickname: '',
+      auth: 'USER',
+    };
+
+    const mockQueryRunner = {
+      commitTransaction: jest.fn(),
+    } as unknown as QueryRunner;
+
+    postService.saveDiffOnPost = jest.fn();
+    postService.saveCurrentSnapshot = jest.fn();
+    postService.createTenMultipleSnapshot = jest.fn();
+
+    // when
+    await postService.savePostAndSnapshot(
+      createPostRecordDto,
+      movieId,
+      user,
+      mockQueryRunner,
+    );
+
+    // then
+    expect(postService.saveDiffOnPost).toHaveBeenCalledTimes(1);
+    expect(postService.saveCurrentSnapshot).toHaveBeenCalledTimes(1);
+    expect(mockQueryRunner.commitTransaction).toHaveBeenCalledTimes(1);
+    expect(postService.createTenMultipleSnapshot).toHaveBeenCalledTimes(1);
+  });
+
+  it('saveDiffOnPost success', async () => {
+    // given
+    const createPostRecordDto: CreatePostRecordDto = {
+      content: '',
+      comment: 'ㅇㅇ',
+      version: null,
+    };
+    const movieId = 1;
+    const user = {
+      userId: 1,
+      email: '',
+      nickname: '',
+      auth: 'USER',
+    };
+
+    const mockQueryRunner = {} as unknown as QueryRunner;
+
+    const movie = {};
+
+    movieRepository.findOneMovie = jest.fn().mockReturnValue(movie);
+    postService.checkLatestVersion = jest.fn();
+    postService.calcDiff = jest.fn();
+
+    // when
+    await postService.saveDiffOnPost(
+      createPostRecordDto,
+      movieId,
+      user,
+      mockQueryRunner,
+    );
+
+    // then
+    expect(movieRepository.findOneMovie).toHaveBeenCalledTimes(1);
+    expect(postRepository.getLatestPostRecord).toHaveBeenCalledTimes(1);
+    expect(postService.checkLatestVersion).toHaveBeenCalledTimes(1);
+    expect(postService.calcDiff).toHaveBeenCalledTimes(1);
+    expect(postRepository.createPostRecord).toHaveBeenCalledTimes(1);
+  });
+
+  it('saveCurrentSnapshot success currentSnapshot exist', async () => {
+    // given
+    const createPostRecordDto: CreatePostRecordDto = {
+      content: '',
+      comment: 'ㅇㅇ',
+      version: null,
+    };
+    const movieId = 1;
+
+    const mockQueryRunner = {} as unknown as QueryRunner;
+
+    const currentSnapshot = {};
+
+    csRepository.findOneCurrentSnapshot = jest
+      .fn()
+      .mockReturnValue(currentSnapshot);
+
+    // when
+    await postService.saveCurrentSnapshot(
+      createPostRecordDto,
+      movieId,
+      mockQueryRunner,
+    );
+
+    // then
+    expect(csRepository.findOneCurrentSnapshot).toHaveBeenCalledTimes(1);
+    expect(csRepository.createCurrentSnapshot).toHaveBeenCalledTimes(0);
+    expect(csRepository.updateCurrentSnapshot).toHaveBeenCalledTimes(1);
+  });
 });
