bec5b7a6b4f459305151c3cbcc6635cd88d3ef10
redjun89 <jjune1816@gmail.com>
2023-06-24 18:58:24 +0900

    [Fix] 버전 기록 전체 조회 diff 데이터 조회 코드 일부 수정

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -202,6 +202,7 @@ export class PostService {
       }
 
       const latestPost = await this.postRepository.getPostRecords(movieId);
+      // console.log(latestPost);
 
       const result = [];
 
@@ -212,8 +213,10 @@ export class PostService {
 
         const diffs = await this.postRepository.findDiffsByVersion(movieId, i);
         // console.log('getPostRecords diffs :', diffs);
+        
         const post = await this.postRepository.findPostByVersion(movieId, i);
         // console.log('getPostRecords post :', post);
+        
         const diffUtil = new DiffUtil();
         let content = original.content;
         for (let j = 0; j < diffs.length; j++) {
@@ -230,11 +233,11 @@ export class PostService {
           diff: JSON.parse(post.content),
         });
       }
-      // console.log(result);
       const end = performance.now();
       const duration = end - start;
 
       console.log(`코드 실행 시간: ${duration}ms`);
+      // console.log(result);
       return result;
     } catch (error) {
       throw new HttpException('수정 기록 조회에 실패했습니다.', 400);
