18f9f63b33dbcc9d263e5d02cf6ffe0b8a531c94
dhtjddls <dhtjddls12@naver.com>
2023-06-25 22:54:51 +0900

    fix delete npx, npm file

--- src/app-cluster.service.ts
+++ src/app-cluster.service.ts
@@ -1,24 +1,25 @@
+import { Injectable } from '@nestjs/common';
 import cluster from 'cluster';
 import os from 'os';
-import { Injectable } from '@nestjs/common';
 
 const numCPUs = os.cpus().length;
 
 @Injectable()
 export class AppClusterService {
-    static clusterize(callback: Function): void {
-        if(cluster.isPrimary){
-            console.log(`Master server started on ${process.pid}`);
-            for (let i = 0; i < numCPUs; i++) {
-                cluster.fork();
-            }
-            cluster.on('exit', (worker, code, signal) => {
-                console.log(`Worker ${worker.process.pid} died. Restarting`);
-                cluster.fork();
-            })
-        } else {
-            console.log(`Cluster server started on ${process.pid}`)
-            callback();
-        }
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  static clusterize(callback: Function): void {
+    if (cluster.isPrimary) {
+      console.log(`Master server started on ${process.pid}`);
+      for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+      }
+      cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died. Restarting`);
+        cluster.fork();
+      });
+    } else {
+      console.log(`Cluster server started on ${process.pid}`);
+      callback();
     }
+  }
 }
